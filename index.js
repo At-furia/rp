fc07460bb4b6b0c672b8ae3fb762d6ad57b73708
At-furia <alexandre.1608@hotmail.fr>
2019-01-27 17:03:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on('message', message => {
     }
     
     
-     let guild = message.member.guild;
+     let guild = 538867865533677569;
     let Humain = guild.roles.find(role => role.id === 538868210984943666);
     let Robot = guild.roles.find(role => role.id === 538868260989435924);
 
