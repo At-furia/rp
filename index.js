f29a6a9f4bbde6e661fb51890b4cb8d168aac349
At-furia <alexandre.1608@hotmail.fr>
2019-01-27 17:17:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,20 +46,21 @@ bot.on('message', message => {
     }
     
     
-     let guild = "538867865533677569";
-    //let Humain = guild.roles.get("538868210984943666");
+    let guild = message.member.guild;
+    let Humain = guild.roles.find(role => role.id === "538868210984943666");
     let Robot = guild.roles.find(role => role.id === "538868260989435924");
 
-    if (message.channel.type === 'dm') {
           if (message.content.startsWith(prefix + "humain")) {
-              message.reply("Vous rejoignez le camp des Humains")
+             // message.reply("Vous rejoignez le camp des Humains")
                       message.member.addRole(Humain);
+         message.delete()
 
     }
         
      if (message.content.startsWith(prefix + "robot")) {
-              message.reply("Vous rejoignez le camp des Robots")
+             // message.reply("Vous rejoignez le camp des Robots")
+         message.delete()
                  message.member.addRole(Robot);
 
-    }}
+    }
     })
