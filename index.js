bbfc0081beed82699fea1936abfa465c6e29cee8
At-furia <alexandre.1608@hotmail.fr>
2019-02-10 20:18:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const adapter = new FileSync('database.json');
 const db = low(adapter);
 
+db.defaults({objet: []}).write()
 
 var prefix = "!"
 var bot = new Discord.Client();
